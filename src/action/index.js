f47874728d525e0ebffd4fbd8785e16c19bfb642
sanika <sanika2110118@akgec.ac.in>
2022-12-04 23:18:15 +0530

    Final commit

--- src/action/index.js
+++ src/action/index.js
@@ -2,7 +2,7 @@ export const addItem =(element)=>{
 return{
     type:"ADDITEM",
     payload:{
-        id:Math.floor(Math.random()*5),
+        id:Math.floor(Math.random()*10)+1,
         element:element
     }
 }
